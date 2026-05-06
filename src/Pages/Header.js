302af32b856d4ac925397b6bdfc97d875f57789b
Siva <naveen.v.zeeroli@gmail.com>
2026-05-06 14:50:46 +0530

    P2P added

--- src/Pages/Header.js
+++ src/Pages/Header.js
@@ -541,13 +541,13 @@ const Header = () => {
                         >
                           Staking
                         </Link> */}
-                        {/* <Link
+                        <Link
                           to={isAuthenticated ? "/p2p" : "/login"}
                           color="inherit"
                           className="contact_button  uppercase font-bold"
                         >
                           {t("p2p")}
-                        </Link> */}
+                        </Link>
 
                         {/* <Link
                           to="/Checkout"
@@ -557,14 +557,14 @@ const Header = () => {
                           {t("fiat_deposit")}
                         </Link> */}
 
-                        {/* <Link
+                        <Link
                           to="/internaltransfer"
                           color="inherit"
                           className="contact_button  uppercase font-bold"
                         >
                           {t("internal_transfer")}
                         </Link>
-                        <Link
+                       {/*  <Link
                           to="/fundtransfer"
                           color="inherit"
                           className="contact_button  uppercase font-bold"
